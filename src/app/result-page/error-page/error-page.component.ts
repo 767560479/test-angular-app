4b2933aa166d2455e121a3d4a1b76a45c44de1f7
谭正飞 <767560479@qq.com>
2019-12-31 10:45:17 +0800

    [修改]错误页面

--- src/app/result-page/error-page/error-page.component.ts
+++ src/app/result-page/error-page/error-page.component.ts
@@ -9,33 +9,14 @@ import { DfNetworkOptions } from 'src/app/models/df-network.model';
   styleUrls: ['./error-page.component.scss']
 })
 export class ErrorPageComponent implements OnInit {
-  isCollapsed = false;
-  isReverseArrow = false;
-  width: string | number = '200px';
+
 
   constructor(
     private http: AngularNetworkService,
   ) { }
 
   ngOnInit() {
-    this.test1();
-    // tslint:disable-next-line:only-arrow-functions
-    $('button').on('click',  function() {
-      alert('xxxxxx');
-    });
   }
-
-
-  test1() {
-    this.test().subscribe(data => {});
-  }
-
-  test() {
-    const params: DfNetworkOptions = { url: '/gateway/hr-common-service/fndLookup/types/queryFndLookupList', method: 'post' };
-    return this.http.fullRequest(params, {}, {});
-  }
-
-
 
 
 }
